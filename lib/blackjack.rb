789a8478265f75c1481dc97f80188c903908fd7e
Chett Tiller <chett.tiller@gmail.com>
2018-09-21 22:50:44 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,7 @@ def runner
   welcome
   initial_round($card_total)
   display_card_total($card_total)
-  until card_total > 21
+  until $card_total > 21
     hit?($card_total)
     display_card_total($card_total)
   end
