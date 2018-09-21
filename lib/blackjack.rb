46346d5013b1ecc986f7a902427a2c9e463b58dd
Chett Tiller <chett.tiller@gmail.com>
2018-09-21 22:24:25 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,8 @@ def hit?(card_total)
   if input == 's'
     card_total
   elsif input == 'h'
-    card_total += deal_card
-    display_card_total(card_total)
-    card_total
+    add = deal_card
+    card_total += add
   else
     invalid_command
     prompt_user
