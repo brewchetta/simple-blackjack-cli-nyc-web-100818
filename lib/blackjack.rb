a54a6374e3b35778d5b4ce1178e245037821b929
Chett Tiller <chett.tiller@gmail.com>
2018-09-21 22:39:04 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,8 +54,8 @@ end
 def runner
   card_total = 0
   welcome
-  initial_round(card_total)
-  puts card_total
+  initial_round
+  display_card_total(card_total)
   until card_total > 21
     hit?(card_total)
     display_card_total(card_total)
